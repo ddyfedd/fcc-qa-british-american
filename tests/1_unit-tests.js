b2ddcbc2bfcfb40dfae67721726cb22701edf3b1
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-04-16 20:38:38 +0200

    american translations

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -59,20 +59,63 @@ suite('Unit Tests', () => {
   });
 
   suite('Translate to American English', () => {
-    test("Translate: We watched the footie match for a while. to American English", (done) => {});
-    test("Translate: Paracetamol takes up to an hour to work. to American English", (done) => {});
-    test("Translate: First, caramelise the onions. to American English", (done) => {});
-    test("Translate: I spent the bank holiday at the funfair. to American English", (done) => {});
-    test("Translate: I had a bicky then went to the chippy. to American English", (done) => {});
-    test("Translate: I've just got bits and bobs in my bum bag. to American English", (done) => {});
-    test("Translate: The car boot sale at Boxted Airfield was called off. to American English", (done) => {});
-    test("Translate: Have you met Mrs Kalyani? to American English", (done) => {});
-    test("Translate: Prof Joyner of King's College, London. to American English", (done) => {});
-    test("Translate: Tea time is usually around 4 or 4.30. to American English", (done) => {});
+    test("Translate: We watched the footie match for a while. to American English", (done) => {
+      assert.equal(translator.toAmericanEnglish("We watched the footie match for a while.")[0], 
+      "We watched the soccer match for a while.");
+      done();
+    });
+    test("Translate: Paracetamol takes up to an hour to work. to American English", (done) => {
+      assert.equal(translator.toAmericanEnglish("Paracetamol takes up to an hour to work.")[0], 
+      "Tylenol takes up to an hour to work.");
+      done();
+    });
+    test("Translate: First, caramelise the onions. to American English", (done) => {
+      assert.equal(translator.toAmericanEnglish("First, caramelise the onions.")[0], 
+      "First, caramelize the onions.");
+      done();
+    });
+    test("Translate: I spent the bank holiday at the funfair. to American English", (done) => {
+      assert.equal(translator.toAmericanEnglish("I spent the bank holiday at the funfair.")[0], 
+      "I spent the public holiday at the carnival.");
+      done();
+    });
+    test("Translate: I had a bicky then went to the chippy. to American English", (done) => {
+      assert.equal(translator.toAmericanEnglish("I had a bicky then went to the chippy.")[0], 
+      "I had a cookie then went to the fish-and-chip shop.");
+      done();
+    });
+    test("Translate: I've just got bits and bobs in my bum bag. to American English", (done) => {
+      assert.equal(translator.toAmericanEnglish("I've just got bits and bobs in my bum bag.")[0], 
+      "I've got odds and ends in my fanny pack.");
+      done();
+    });
+    test("Translate: The car boot sale at Boxted Airfield was called off. to American English", (done) => {
+      assert.equal(translator.toAmericanEnglish("The car boot sale at Boxted Airfield was called off.")[0], 
+      "The swap meet at Boxted Airfield was called off.");
+      done();
+    });
+    test("Translate: Have you met Mrs Kalyani? to American English", (done) => {
+      assert.equal(translator.toAmericanEnglish("Have you met Mrs Kalyani?")[0], 
+      "Have you met Mrs. Kalyani?");
+      done();
+    });
+    test("Translate: Prof Joyner of King's College, London. to American English", (done) => {
+      assert.equal(translator.toAmericanEnglish("Prof Joyner of King's College, London.")[0], 
+      "Prof. Joyner of King's College, London.");
+      done();
+    });
+    test("Translate: Tea time is usually around 4 or 4.30. to American English", (done) => {
+      assert.equal(translator.toAmericanEnglish("Tea time is usually around 4 or 4.30.")[0], 
+      "Tea time is usually around 4 or 4:30.");
+      done();
+    });
   });
 
   suite('Highlight translation', () => {
-    test('Highlight: Mangoes are my favorite fruit.', (done) => {});
+    test('Highlight: Mangoes are my favorite fruit.', (done) => {
+      assert.equal();
+      done();
+    });
     test('Highlight: I ate yogurt for breakfast.', (done) => {});
     test('Highlight: We watched the footie match for a while.', (done) => {});
     test('Highlight: Paracetamol takes up to an hour to work.', (done) => {});
